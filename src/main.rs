1c81699da2701ea6cfb8791f7f4fc4005d5acde6
TurtleMeds <owenwells2009@gmail.com>
2024-01-16 19:39:08 -0800

    added hello world

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,12 @@
-use core::panic::PanicInfo;
-
 // Disable standard library
 #![no_std]
 // Overwrite entry point
 #![no_main]
 
-// PANIC HANDLER
-#[panic_handler]
-// this function is called on panic
-fn panic(_info: &PanicInfo) -> {
-    loop{}
-}
+use core::panic::PanicInfo;
+
+
+static HELLO: &[u8] = b"Hello, World";
 
 // disable name mangling so the "_start" function is actually named "_start" and not
 // changed into random letters and numbers
@@ -21,5 +17,19 @@ fn panic(_info: &PanicInfo) -> {
 // most systems
 pub extern "C" fn _start() -> ! { // the return type "!" means the function is never
                                   // allowed to return
+    let vga_buffer = 0xb8000 as *mut u8;
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe{
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+    loop{}
+}
+
+// PANIC HANDLER
+#[panic_handler]
+// this function is called on panic
+fn panic(_info: &PanicInfo) -> ! {
     loop{}
 }
