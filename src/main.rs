0999e8907c3c72e16989317a5f076ec15f686df4
TurtleMeds <owenwells2009@gmail.com>
2024-01-19 12:50:45 -0800

    got printing to screen functions working finally! added extra features to build-std and added test printing function in vga-buffer

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,6 @@ mod vga_buffer;
 
 use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"Hello, World";
 
 // disable name mangling so the "_start" function is actually named "_start" and not
 // changed into random letters and numbers
@@ -18,13 +17,8 @@ static HELLO: &[u8] = b"Hello, World";
 // most systems
 pub extern "C" fn _start() -> ! { // the return type "!" means the function is never
                                   // allowed to return
-    let vga_buffer = 0xb8000 as *mut u8;
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe{
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    vga_buffer::print_smth();
+
     loop{}
 }
 
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -87,4 +87,13 @@ impl Writer {
     }
     fn newline(&mut self) {/* TODO */}
 }
-
+pub fn print_smth() {
+    let mut writer = Writer {
+        column_position: 0,
+        color_code: ColorCode::new(Color::Yellow, Color::Black),
+        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+    };
+    writer.write_byte(b'H');
+    writer.write_string("ello ");
+    writer.write_string("Wörld!");
+}
