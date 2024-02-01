dc6786a500dcf3464e839963c878e24c2e30e8c3
TurtleMeds <owenwells2009@gmail.com>
2024-01-31 17:58:57 -0800

    Completely refactored testing. made a library and tests directory.

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,12 @@
 #![no_std]
 // Overwrite entry point
 #![no_main]
-
 #![feature(custom_test_frameworks)]
-
-#![test_runner(crate::test_runner)]
-
+#![test_runner(rust_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-mod vga_buffer;
-mod serial;
-
 use core::panic::PanicInfo;
-
+use rust_os::println;
 
 // disable name mangling so the "_start" function is actually named "_start" and not
 // changed into random letters and numbers
@@ -37,7 +31,6 @@ pub extern "C" fn _start() -> ! { // the return type "!" means the function is n
 // PANIC HANDLER
 #[cfg(not(test))]
 #[panic_handler]
-// and this one in non-test mode
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
@@ -45,51 +38,6 @@ fn panic(info: &PanicInfo) -> ! {
 
 #[cfg(test)]
 #[panic_handler]
-// and this one in test mode
 fn panic(info: &PanicInfo) -> ! {
-    serial_println!("[failed]\n");
-    serial_println!("Error: {}\n", info);
-    exit_qemu(QemuExitCode::Failed);
-    loop{}
-}
-// TESTING
-
-pub trait Testable {
-    fn run(&self) -> ();
-}
-impl<T> Testable for T where T: Fn(), {
-    fn run(&self) -> () {
-        serial_print!("{}...\t", core::any::type_name::<T>());
-        self();
-        serial_println!("[ok]");
-    }
-}
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11,
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
-}
-
-#[cfg(test)]
-pub fn test_runner(tests: &[&dyn Testable]) {
-    println!("Running {} tests...", tests.len());
-    for test in tests {
-        test.run();
-    }
-    exit_qemu(QemuExitCode::Success);
-}
-
-#[test_case]
-fn trivial_assertion() {
-    assert_eq!(1, 1);
+    rust_os::test_panic_handler(info)
 }
